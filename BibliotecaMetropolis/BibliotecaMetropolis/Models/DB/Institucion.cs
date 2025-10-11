f0bc16db1f1f0cc90161a1a40354a8db9d24a095
DavidRivera011 <62643408+DavidRivera011@users.noreply.github.com>
2025-10-11 00:03:20 -0600

    Modelos finales generados mediante Entity Framework a partir de la base de datos BibliotecaMetropolis diseñada en SQL Server.
    Modelos generados con Entity Framework a partir de la base de datos BibliotecaMetropolis.
    Se incluye el archivo .sql para la creación completa de la base de datos.

---
+++ BibliotecaMetropolis/BibliotecaMetropolis/Models/DB/Institucion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BibliotecaMetropolis.Models.DB;
+
+public partial class Institucion
+{
+    public int IdInstitucion { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public virtual ICollection<Material> Materials { get; set; } = new List<Material>();
+}
